d3eb0aadec7bdb8d60935abf8dcdbd653af6d474
Marcin Szymczak <szymczakmchtr@gmail.com>
2024-10-20 20:13:04 +0200

    plots

--- cw1.py
+++ cw1.py
@@ -16,22 +16,40 @@
 from autograd import grad
 import numpy as np
 from cec2017.functions import f3, f12
+import matplotlib.pyplot as plt
 
 #funkcja kwadratowa
 def f(x):
-    return x ** 2
+    return sum(x ** 2)
 
-def solver(f, x0, alfa=1e-2, epsilon=1e-6):
+def solver(f, x0, alfa=1e-3, epsilon=1e-1):
     gradient = grad(f)
     x = x0
+    f_values = []
 
     while True:
+
         gradient_value = gradient(x)
         x_new = x - alfa * gradient_value
-        if abs(gradient_value) <= epsilon or abs(x_new - x) <= epsilon:
+        f_values.append(f(x_new))
+
+        if np.all(abs(gradient_value)) <= epsilon or np.all(abs(x_new - x)) <= epsilon:
+            print("Znaleziono!")
             break
+
         x = x_new
-        print(x)
-    return x
-print(solver(f, -0.5))
+    return x, f_values
+
+def plot(f_values, alfa):
+    plt.plot(f_values)
+    plt.title(f"Zbieżność dla alfa={alfa}")
+    plt.xlabel("Iteracja")
+    plt.ylabel("Wartość funkcji celu")
+    plt.grid(True)
+    plt.show()
+
+x0 = np.random.uniform(-100.0, 100.0, 10)
+x, f_values = solver(f, x0)
+print(x)
+plot(f_values, 1e-1)
 
