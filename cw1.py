24b1b275a779e477316734a5d34fb2387daaefd3
Marcin <szymczakmchtr@gmail.com>
2024-10-24 20:10:17 +0200

    Final version

--- cw1.py
+++ cw1.py
@@ -17,6 +17,8 @@ from autograd import grad
 import autograd.numpy as np
 from cec2017.functions import f3, f12
 import matplotlib.pyplot as plt
+import time 
+import json
 
 #quadratic function
 def f(x):
@@ -53,16 +55,27 @@ def solver(f, x0, alfa=1e-3, epsilon=1e-6, iterations=3000):
 
 #plot drawing
 def plot(f, x0, alfa_to_test):
+    evaluation_times = {}
     for alfa in alfa_to_test:
+        start_time = time.time()
         result = solver(f, x0, alfa)
+        end_time = time.time()
+        evaluetion_time = end_time - start_time
+        evaluation_times[alfa] = round(evaluetion_time, 3)
         plt.plot(result['f_values'], label=f'alfa={alfa}')
-    
+    #log-lin plot
+    #plt.yscale('log')
+    with open('evaluation_times.json', 'w') as file:
+        json.dump(evaluation_times, file)
+
     plt.xlabel('Iteracje')
     plt.ylabel('Wartość funkcji')
-    plt.title('Wykresy zbieżności dla różnych parametrów kroków')
+    plt.title('Wykresy zbieżności dla różnych wartości parametru kroku')
     plt.legend(bbox_to_anchor=(1, 0.5))
     plt.grid(True)
     plt.show()
+    
+
 
 #Note that each function takes a 2D array and returns a 1D array
 def f3_adapter(x):
@@ -72,7 +85,7 @@ def f3_adapter(x):
 
 
 
-alfa_to_test = [1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+alfa_to_test = [1e-10, 1e-9, 1e-8] #[1e-3, 1e-2, 1e-1]
 x0_cec = np.random.uniform(-100.0, 100.0, size=(1, 10))
 
 
